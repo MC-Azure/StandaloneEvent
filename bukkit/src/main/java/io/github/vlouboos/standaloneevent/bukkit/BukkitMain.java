eefd02f42b58e900d39f3d4f4ffb056d718bd810
vlouboos <vlouyearlinjinhua@outlook.com>
2025-12-30 17:40:49 +0800

    *license

--- bukkit/src/main/java/io/github/vlouboos/standaloneevent/bukkit/BukkitMain.java
+++ bukkit/src/main/java/io/github/vlouboos/standaloneevent/bukkit/BukkitMain.java
@@ -1,5 +1,5 @@
 // This file is part of StandaloneEvent.
-// You WON'T be guaranteed to be permitted with this file unless you're under BSD-3 Licence.
+// You WON'T be guaranteed to be permitted with this file unless you're under BSD-3 License.
 // See https://spdx.org/licenses/BSD-3-Clause.html
 package io.github.vlouboos.standaloneevent.bukkit;
 
